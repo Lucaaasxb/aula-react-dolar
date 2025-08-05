2fce818b823cbee8b3242a6a022804c9c076d1b8
Lucas <lucasbriansilvafagundes@gmail.com>
2025-08-05 20:00:56 -0300

    adição do bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
     <>
      <h1 className='display-3'>Calculadora de dólar</h1>
      <img src="/public/dollar.png" width='50'/>
+     <br/>
+     <button className='btn btn-primary'>Click Aqui</button>
     </>
   )
 }
