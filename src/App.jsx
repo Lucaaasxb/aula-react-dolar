c9acea766bd5b766e3cea4edd9c8169ee0fc755a
Lucas <lucasbriansilvafagundes@gmail.com>
2025-08-05 20:18:06 -0300

    implementação do componente titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css'
+import Titulo from "./titulo"
 
 function App() {
  
 
   return (
     <>
-     <h1 className='display-3'>Calculadora de dólar</h1>
-     <img src="/public/dollar.png" width='50'/>
-     <br/>
-     <button className='btn btn-primary'>Click Aqui</button>
+     <div className='container'>
+     <Titulo />
+     </div>
     </>
   )
 }
---
+++ src/titulo.jsx
@@ -0,0 +1,14 @@
+import './Titulo.css'
+
+function Titulo() {
+    return(
+        <>
+        <div className='text-center'>
+            <h1 className='display-3'>Calculadora de dólar</h1>
+            <img src="/public/dollar.png" width='50'/>
+        </div>
+        </>
+    )
+}
+
+export default Titulo
