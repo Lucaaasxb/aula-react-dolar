ec35402019c435fa7b98b1d9ddc36e6d0ec9e8dc
Lucas <lucasbriansilvafagundes@gmail.com>
2025-08-07 21:45:18 -0300

    arrrumação de bugs do calDolar

--- src/CalcDolar.jsx
+++ src/CalcDolar.jsx
@@ -1,4 +1,5 @@
 import './CalcDolar.css'
+import { FaMoneyBillTransfer } from "react-icons/fa6";
 
 function CalcDolar() {
 
@@ -6,21 +7,21 @@ function CalcDolar() {
         const reais = document.getElementById("reais").value
         const cotacao = document.getElementById("cotacao").value
         const total = reais / cotacao
-        alert(total)
+        alert("O valor convertido para dólares é $" + total)
     })
 
     return (
         <>
             <form className='w-25 m-auto pt-5 pb-5'>
-                <div className='form-group' mb-3>
+                <div className="form-group mb-3">
                     <label htmlFor='reais'>Valor em Reais</label>
                     <input type='text' className='form-control' id='reais' />
                 </div>
-                <div className='form-group' mb-3>
+                <div className="form-group mb-3">
                     <label htmlFor='cotacao'>Cotação do Dólar</label>
                     <input type='text' className='form-control' id='cotacao' />
                 </div>
-                <button type='button' onClick={Calcular} className='btn btn-primary w-100'>Calcular</button>
+                <button type='button' onClick={Calcular} className='btn btn-primary w-100' ><FaMoneyBillTransfer size={25} color='black' /> Calcular</button>
             </form>
         </>
     )
